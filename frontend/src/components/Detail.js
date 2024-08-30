feedf2d5be836a05faeed73bb11d0ef7e81cf339
mj <ui2939@gmail.com>
2024-08-30 22:36:21 +0900

    feat : detail page first UI #46

---
+++ frontend/src/components/Detail.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Detail.css";
+
+function Detail() {
+
+
+    return (
+        <div
+            className="detail-container">
+                <div id="top-bar"></div>
+                <h2>화장실 이름</h2>
+        </div>
+    );
+}
+
+export default Detail;
